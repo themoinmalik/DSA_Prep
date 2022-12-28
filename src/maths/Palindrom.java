80e23d5cb2d96851da04d0c917b27712aab9dcea
Moin Malik <Moin Malik>
2022-12-28 21:43:37 +0530

    reverse and palindrom

---
+++ src/maths/Palindrom.java
@@ -0,0 +1,24 @@
+package src.maths;
+
+public class Palindrom {
+    public static void main(String[] args) {
+        
+        int num = 121;
+        int reverse = ReverseNum(num);
+        System.out.println(reverse == num);
+    }
+
+    private static int ReverseNum(int num) {
+
+        int rev = 0;
+
+        while (num!=0) {
+            int rem = num%10;
+            rev = rev*10 + rem;
+            num = num/10;
+        }
+        return rev;
+    }
+
+
+}
