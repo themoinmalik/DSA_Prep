c05b4a4fa272e1ac906b9236e0f7427cf325832e
Moin Malik <Moin Malik>
2022-12-29 16:08:02 +0530

    update

--- src/maths/Palindrom.java
+++ src/maths/Palindrom.java
@@ -11,7 +11,6 @@ public class Palindrom {
     private static int ReverseNum(int num) {
 
         int rev = 0;
-
         while (num!=0) {
             int rem = num%10;
             rev = rev*10 + rem;
@@ -19,6 +18,4 @@ public class Palindrom {
         }
         return rev;
     }
-
-
 }
