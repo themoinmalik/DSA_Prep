671c871ebfb2f3490bacc3870dcffc708f4c1b14
Moin Malik <Moin Malik>
2023-01-02 22:09:27 +0530

    2 jan

---
+++ src/string/Urilify.java
@@ -0,0 +1,33 @@
+package src.string;
+
+public class Urilify {
+
+    public static void main(String[] args) {
+        String str = "moin malik   ";
+        int trueLen = 10;
+        String ans = replaceStringWithPer20(str,trueLen);
+        System.out.println(ans);
+    }
+
+    private static String replaceStringWithPer20(String str, int trueLen) {
+
+        int spaceCount = 0;
+        // number of spaces
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ' ') {
+                spaceCount++;
+            }
+        }
+
+        int index = trueLen + spaceCount * 2;
+
+        for (int i = 0; i < index; i++) {
+
+            if (str.charAt(i) == ' ') {
+
+            }
+        }
+
+        return str;
+    }
+}
