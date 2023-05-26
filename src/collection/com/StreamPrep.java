f500ae002656f8f1bb4fff96586e6461fd37531a
moin malik <moin.malik@olx.com>
2023-05-26 09:57:02 +0530

    may 26

--- src/collection/com/StreamPrep.java
+++ src/collection/com/StreamPrep.java
@@ -8,17 +8,15 @@ import java.util.stream.Collectors;
 public class StreamPrep {
 
 
-
-
     public static void main(String[] args) {
 
 
-        List<Integer> list = Arrays.asList(4,6,3,4,6,8,2,5,8);
+        List<Integer> list = Arrays.asList(4,6,3,46,63,8,2,5,8,45,7,78,4,67,18,43,96);
 
         // find sum of all the numbers
 
         Optional<Integer> and = list.stream()
-                .reduce((a,b) -> a+b);
+                .reduce(Integer::sum);   // a -> a+b can be replace.
 
         System.out.println(and.get());
 
@@ -40,7 +38,24 @@ public class StreamPrep {
         System.out.println(sqr);
 
 
-        System.out.println("moin malik ");
+        // find odd even numbers from list
+
+        // 1. even
+
+        List<Integer> even = list.stream()
+                .filter(a->a%2==0)
+                .collect(Collectors.toList());
+
+        System.out.println(even);
+
+
+        // 2. odd.
+
+        List<Integer> odd = list.stream()
+                .filter(a->a%2!=0)
+                .collect(Collectors.toList());
+
+        System.out.println(odd);
 
 
 
