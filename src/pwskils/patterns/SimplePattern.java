b04005c2753cb82d45343f9e33976e9b9a5998c6
moin malik <moin.malik@olx.com>
2023-05-30 19:55:11 +0530

    30 may

---
+++ src/pwskils/patterns/SimplePattern.java
@@ -0,0 +1,83 @@
+package src.pwskils.patterns;
+
+public class SimplePattern {
+
+    public static void main(String[] args) {
+
+        // triangle..
+
+//        for (int i = 0;i<=5;i++) {
+//            for (int j = 0;j<=i;j++) {
+//                System.out.print(" * " );
+//            }
+//            System.out.println("");
+//        }
+
+        // reverse ...
+
+        int n = 5;
+
+//        for (int i = 0;i<n;i++) {
+//            for (int j = i;j<n;j++) {
+//                System.out.print(" * ");
+//            }
+//            System.out.println("");
+//        }
+
+        // stairs...
+
+//        for (int i=0;i<n;i++){
+//            for (int j=i;j<n-1;j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println("&");
+//        }
+
+        //   square...
+
+//        for (int i = 0; i <= n; i++) {
+//            for (int j = 0; j <=n; j++) {
+//                if (i == 0 || j == 0 || i == n || j == n) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println("");
+//        }
+
+        // diagonal...
+
+//        for (int i = 0; i <= n; i++) {
+//            for (int j = 0; j <=n; j++) {
+//                if (i == j) {
+//                    System.out.print(" * ");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//            System.out.println("");
+//        }
+
+        // cross.
+
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j <=n; j++) {
+                if (i == j || i+j == n ) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println(" ");
+        }
+
+
+
+
+
+    }
+
+}
+
+
