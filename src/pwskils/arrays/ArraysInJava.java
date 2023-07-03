e25925652fb25b9ad8d0884d986fd585853eb55a
moin malik <moin.malik@olx.com>
2023-07-03 16:15:30 +0530

    3 july

--- src/pwskils/arrays/ArraysInJava.java
+++ src/pwskils/arrays/ArraysInJava.java
@@ -6,39 +6,26 @@ public class ArraysInJava {
 
     // diagnol  summ.. . 
 
-    public static int solution(int[][] arr,int k) {
+    public static int[][] solution(int[][] arr) {
 
-        int sum = 0;
-        int n = arr.length-1;
 
+        // transpose... 
 
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-
-                if (i==j) {
-                    sum = sum + arr[i][j];   
-                }
-                if (j == arr.length - 1-i) {
-                    sum = sum + arr[i][j]; 
-                } 
+            for (int j = i+1; j < arr.length; j++) {
+                arr[j][i] = arr[i][j];        
             }
             
         }
-
-        if (arr.length%2 != 0) {
-            sum = sum - arr[n/2][n/2];
-        }
-        return sum;
- 
-       
+        
+        return arr;
     }
     public static void main(String[] args) {
 
-        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
-        int  k = 1;
-        
-        int ans = solution(arr, k);
-        System.out.println(ans);
+        int[][] arr = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+
+        int[][] ans = solution(arr);
+        System.out.println(Arrays.deepToString(ans));
 
     }
 }
