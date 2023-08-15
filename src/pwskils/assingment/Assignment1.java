b80d7a3a599f454fa2d3ad8df24d584553001dcf
Moin Malik <moin2018ca069@abesit.edu.in>
2023-08-15 17:46:48 +0530

    cyclic sort

--- src/pwskils/assingment/Assignment1.java
+++ src/pwskils/assingment/Assignment1.java
@@ -1,39 +1,52 @@
 package src.pwskils.assingment;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.List;
 
 public class Assignment1 {
-    public static int[] solution(int[] arr) {
 
-        // cyclic sort....
+    public static int[] sort(int[] arr) {
+
+        // cyclic sort.... Algo.
+        // find the missing ...  where n = length and last element also ?
+        // find all number disappear in an Array... ?
+        // find the duplicates.... ?
+
+
+        // sorting using cyclic sort.
 
         int i = 0;
+        int n = arr.length;
 
-        while (i<arr.length) {
-            int correct = arr[i]-1;    // correct = arr[0]  if array start with 0;
-            if (arr[i] != arr[correct]) {
-                swap(arr, i , correct);
+        while (i<n ) {
+            int correct = arr[i]-1;    // correct = arr[i]  if array start with 0;  //
+            if (arr[i] > 0 && arr[i] <= n && arr[i] != arr[correct] ) {   // changes required acc to ques.. .
+                int temp = arr[i];
+                arr[i] = arr[correct];
+                arr[correct] = temp;
             }else {
                 i++;
             }
         }
+
         return arr;
 
-    }
+//        List<Integer> list = new ArrayList<>();
 
-    public static void swap(int[] arr, int i, int c) {
-
-        int temp = arr[i];
-        arr[i] = arr[c];
-        arr[c] = temp;
-
+        // search the element....
+//        for (int j = 0; j < n; j++) {
+//            if (arr[j] !=  j+1) {    // changes with respect to requerement ...
+//                return j+1;
+//            }
+//        }
+//        return n;
     }
 
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 3, 7, 1, 2};
-        int t = 2;
-        int[] ans = solution(arr);
+        int[] arr = {7,8,9,11,12};   // find the missing ....
+        int[] ans = sort(arr);
+//        int ans = sort(arr);
         System.out.println(Arrays.toString(ans));
     }
 }
